417429a5f4e74f66fb3926f8dcd027647febac2e
faizanshaikh2100 <64514340+faizanshaikh2100@users.noreply.github.com>
2022-11-27 10:40:49 +0530

    Time: 25 ms (32.61%), Space: 97.7 MB (47.38%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,8 +8,18 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public ListNode reverse(ListNode head){
+    ListNode reverse(ListNode head){
         ListNode prev=null;
         ListNode curr=head;
         ListNode after;
@@ -19,35 +29,36 @@ class Solution {
             prev=curr;
             curr=after;
         }
-        return prev;
+        head=prev;
+        return head;
     }
-    public ListNode middle(ListNode head){
+    ListNode getMid(ListNode head){
         ListNode slow=head;
         ListNode fast=head;
-        while(fast.next!=null&&fast.next.next!=null){
+        while(fast!=null&&fast.next!=null)
+        {
             slow=slow.next;
             fast=fast.next.next;
         }
         return slow;
     }
+    // ListNode merge(ListNode head1,LisNode head2){
+
+
+
+    // }
     public boolean isPalindrome(ListNode head) {
-        ListNode mid=middle(head);
-        ListNode head2=mid.next;
-        mid.next=null;
-        head2=reverse(head2);
-        
-        ListNode p1=head;
-        ListNode p2=head2;
-        
-        while(p1!=null&&p2!=null){
-            if(p1.val!=p2.val){
+        ListNode firstHead=head;
+        ListNode mid=getMid(head);
+        ListNode secondHead=reverse(mid);
+        while(firstHead!=null&&secondHead!=null){
+            if(firstHead.val!=secondHead.val){
                 return false;
             }
-            p1=p1.next;
-            p2=p2.next;
+            firstHead=firstHead.next;
+            secondHead=secondHead.next;
         }
         return true;
-        
-        
+
     }
 }
