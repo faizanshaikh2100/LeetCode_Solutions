9e3f98cacdd6dfa6dc0ae67179bb7263cbf942cb
faizanshaikh2100 <64514340+faizanshaikh2100@users.noreply.github.com>
2022-11-30 20:38:57 +0530

    Time: 1 ms (12.95%), Space: 49.4 MB (63.10%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,35 +14,27 @@
  * }
  */
 class Solution {
-    public int getMax(TreeNode root){
-        TreeNode temp=root;
-         
-        while(temp.right!=null){
-           temp=temp.right;
-        }
-        int max=temp.val;
-        
-        return max;
+    private int getMin(TreeNode root) {
+                    TreeNode temp=root;
+                    while(temp.left!=null)temp=temp.left;
+                    return temp.val;
+
     }
-   public TreeNode deleteNode(TreeNode root, int key) {
-       if(root==null)return null;
-        if(root.val==key){
-            //1.if root has 0 children
-            if(root.left==null&&root.right==null)return null;
-
-            //2.if root has only left children
-            else if(root.right==null&&root.left!=null)return root.left;
-
-            //3.if root has only right children
-            else if(root.right!=null&&root.left==null)return root.right;
-            
-            int leftMax=getMax(root.left);
-            root.val=leftMax;
-            root.left=deleteNode(root.left,leftMax);
-        } 
-        else if(key<root.val)root.left=deleteNode(root.left,key);
-        else root.right=deleteNode(root.right,key);       
-
-        return root;
+    public TreeNode deleteNode(TreeNode root, int val) {
+                 if(root==null)return null;
+                 if(val<root.val)root.left=deleteNode(root.left,val);
+                 else if(val>root.val) root.right=deleteNode(root.right,val);
+                 else{
+                     if(root.right==null)return root.left;
+                     else if(root.left==null) return root.right;
+                     else if(root.left==null&&root.right==null)
+                         return null;
+                     else {
+                         int rightSuccesor = getMin(root.right);
+                         root.val = rightSuccesor;
+                         root.right = deleteNode(root.right, rightSuccesor);
+                     }
+                 }
+                     return root;
     }
 }
